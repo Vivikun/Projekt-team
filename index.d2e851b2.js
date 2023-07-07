8b78d32d676af6fd34572f682995d3ff3417eb90
Vivikun <129865999+Vivikun@users.noreply.github.com>
2023-07-07 20:04:35 +0000

    Deploying to gh-pages from @ Vivikun/Projekt-team@5f022c31d663dcf544356f76187c6f23e6ea51e5 🚀

---
+++ index.d2e851b2.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-him]"),closeModalBtn:document.querySelector("[data-modal-close-him]"),modal:document.querySelector("[data-modal-him]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.d2e851b2.js.map
